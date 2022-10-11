6eedde17e2963114207db1be29579942ef432650
Mostafa <mostafa.hamed@vodafone.com>
2022-10-11 23:08:51 +0200

    feat: Printed Hobbies

--- WH-FE-NG-TEST_updated/WH-FE-NG-TEST/npm-tests/test-02.js
+++ WH-FE-NG-TEST_updated/WH-FE-NG-TEST/npm-tests/test-02.js
@@ -1,15 +1,22 @@
 //@ts-check
-/** 
+/**
  * run from root folder as : node ./npm-tests/test-02.js
- * 
+ *
  * Parse the response from the given REST end point and print out "hobbies" property in the following format: ITEM1, ITEM2, ...
  */
-import https from "https";
+import https from 'https';
 
+https.get(
+  'https://coderbyte.com/api/challenges/json/rest-get-simple',
+  (resp) => {
+    let data = '';
 
-https.get('https://coderbyte.com/api/challenges/json/rest-get-simple', (resp) => {
-    let data = "";
-    
     // parse json and print "hobbies" property as ITEM1, ITEM2,...
-    console.log();
-})
+    resp.setEncoding('utf-8');
+    resp.on('data', (chunk) => (data += chunk));
+    resp.on('end', () => {
+      const user = JSON.parse(data);
+      console.log(user.hobbies.join(', '));
+    });
+  }
+);
