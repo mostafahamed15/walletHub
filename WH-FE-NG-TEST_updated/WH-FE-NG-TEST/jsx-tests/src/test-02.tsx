f890943cab3dc53132807c353c944875429dee17
Mostafa <mostafa.hamed@vodafone.com>
2022-10-11 21:29:07 +0200

    feat: Increment Counter

--- WH-FE-NG-TEST_updated/WH-FE-NG-TEST/jsx-tests/src/test-02.tsx
+++ WH-FE-NG-TEST_updated/WH-FE-NG-TEST/jsx-tests/src/test-02.tsx
@@ -1,27 +1,41 @@
 /**
- * In the following React template, modify the component so that the counter correctly displays and it increments by one whenever the button is pressed. 
+ * In the following React template, modify the component so that the counter correctly displays and it increments by one whenever the button is pressed.
  * You are free to add classes and styles, but make sure you leave the element ID's as they are.
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
+interface initState {
+  count: number;
+}
 class Counter extends React.Component {
+  state: initState;
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    this.setState((previousValue: initState) => ({
+      count: previousValue.count + 1,
+    }));
   }
 
   render() {
     return (
       <div id="mainArea">
-        <p>button count: <span>0</span></p>
-        <button id="mainButton">Increase</button>
+        <p>
+          button count: <span>{this.state.count}</span>
+        </p>
+        <button id="mainButton" onClick={this.increment}>
+          Increase
+        </button>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Counter />,
-  document.getElementById('test-02')
-);
+ReactDOM.render(<Counter />, document.getElementById('test-02'));
